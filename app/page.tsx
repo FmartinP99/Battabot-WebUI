d266202a18aa86e826bf0cad43327c3c06bbcdb9
Fülep Martin Patrik <f.martin.p99@gmail.com>
2025-11-15 21:31:32 +0100

    metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import Header from "./_components/Header";
 import MainView from "./_components/MainView";
 
+export const metadata = {
+  title: "Battabot Web UI",
+};
+
 export default function Page() {
   return (
     <div className="relative z-10 text-center">
